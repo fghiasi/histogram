b11d4a480b2e4dbb646b8d615e9f11c8f3aafcbd
Farbod Ghiasi <fghiasi@uci.edu>
2022-09-08 11:08:00 -0700

    Add comments for all the methods in script.js

--- src/script.js
+++ src/script.js
@@ -7,17 +7,20 @@ const showBtn = document.getElementById("show");
 const resetBtn = document.getElementById("reset");
 const updateBtn = document.getElementById("update");
 
-
+// Runs after html and css are loaded
 window.onload = function (){
     canvas = document.getElementById("histogram");
     ctx = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
     const randomNums = generateRandomDataset(randomSize)
     histogram = new Histogram(ctx, randomNums , canvas.width, canvas.height);
     histogram.drawAllBars();
 }
 
-
+// Resize event to make the screen responsive
 window.addEventListener('resize', () => {
     canvas = document.getElementById("histogram");
     ctx = canvas.getContext("2d");
@@ -29,7 +32,7 @@ window.addEventListener('resize', () => {
     histogram.drawAllBars();
 });
 
-
+// Update button event
 updateBtn.addEventListener('click', () => {
     const userInput = document.getElementById("input").value;
 
@@ -42,6 +45,7 @@ updateBtn.addEventListener('click', () => {
     }
 });
 
+// Show button event
 showBtn.addEventListener('click', () => {
     const userInput = document.getElementById("input").value;
 
@@ -55,7 +59,7 @@ showBtn.addEventListener('click', () => {
     }
 });
 
-
+// Reset button event
 resetBtn.addEventListener('click', () => {
     histogram.clearCanvas();
     histogram.clearDataset();
@@ -63,7 +67,7 @@ resetBtn.addEventListener('click', () => {
     document.getElementById("input").value = "";
 });
 
-
+// Helper to generate random numbers for home page
 function generateRandomDataset(datasetSize)
 {
     const dataArray = [];
@@ -74,11 +78,12 @@ function generateRandomDataset(datasetSize)
     return dataArray
 }
 
-
+// Histogram interface
 class Histogram {
 
-    yAxisBinsCount = 10;
+    yAxisLineCount = 10;
 
+    // Both x and y axis line width
     axisLinesWidth = 10;
     canvasWidthOffset = 150;
 
@@ -89,15 +94,17 @@ class Histogram {
     barOffsetFromXAxis = this.xAxisOffsetFromCanvasHeight + this.axisLinesWidth;
     barOffsetFromYAxis = this.yAxisOffsetFromCanvasXOrigin + this.axisLinesWidth;
 
-
+    // Canvas context
     #ctx;
     #canvasWidthVal;
     #canvasHeightVal;
 
+    // Sorted Array Data Structure for computing frequencies.
     #dataFrequency;
     #dataFrequencyMax;
     #dataFrequencyMin;
 
+    // Array Data Structure for user input.
     #dataset;
     #datasetMin;
     #datasetMax;
@@ -106,17 +113,19 @@ class Histogram {
 
     constructor(ctx, dataset, canvasWidth, canvasHeight) {
         this.#ctx = ctx;
-        this.#dataset = dataset;
         this.#canvasWidthVal = canvasWidth-this.canvasWidthOffset;
         this.#canvasHeightVal = canvasHeight;
-        this.#datasetMin = Math.min(...this.#dataset);
-        this.#datasetMax = Math.max(...this.#dataset);
 
-        this.calculateFrequency();
+        this.#dataset = dataset;
+        this.#updateDatasetMaxValue(Math.max(...this.#dataset));
+        this.#updateDatasetMinValue(Math.min(...this.#dataset));
+
+        this.#dataFrequency = this.computeFrequency();
         this.#updateDataFrequencyMinValue(Math.min(...this.#dataFrequency));
         this.#updateDataFrequencyMaxValue(Math.max(...this.#dataFrequency));
     }
 
+    // General Interface to draw bars.
     drawAllBars()
     {
         this.clearCanvas();
@@ -124,21 +133,26 @@ class Histogram {
             return;
         }
 
+        // draw axis lines and numbers
         this.drawAxisLines();
-        this.#drawYAxisLabels();
-        this.#drawXAxisLabels();
+        this.drawAxisNumbers();
 
         let x = this.barOffsetFromYAxis;
 
-        for (let binNum = 0; binNum < this.#dataFrequencySize(); binNum++) {
+        // Iterate from 0 to dataFrequency size that is the number of bars in Canvas.
+        for (let barNum = 0; barNum < this.#dataFrequencySize(); barNum++) {
+
             const canvasHeightWithOffset =
                 this.#canvasHeight() - this.barOffsetFromXAxis - this.barOffsetFromCanvasOrigin;
 
+            // Height of each bar relative to max frequency and Canvas height with offsets
             const barHeight =
-                (this.#dataFrequency[binNum]/this.#dataFrequencyMax) * (canvasHeightWithOffset);
+                (this.#dataFrequency[barNum]/this.#dataFrequencyMax) * (canvasHeightWithOffset);
 
-            this.#selectBinRandomColor(binNum, barHeight);
+            // Select random color for each bin
+            this.#selectBinRandomColor();
 
+            // Draw one bar.
             this.#drawOneBar(
                 x,
                 canvasHeightWithOffset - barHeight + this.barOffsetFromCanvasOrigin,
@@ -150,14 +164,28 @@ class Histogram {
 
     }
 
-    calculateFrequency()
+    // Updates the dataset and frequency data as well as min and max values.
+    updateDatasetAndDataFrequency(newDatasetArray)
+    {
+        this.#dataset = this.#dataset.concat(...newDatasetArray);
+        this.#updateDatasetMaxValue(Math.max(...this.#dataset));
+        this.#updateDatasetMinValue(Math.min(...this.#dataset));
+
+        this.#dataFrequency = this.computeFrequency();
+        this.#updateDataFrequencyMinValue(Math.min(...this.#dataFrequency ));
+        this.#updateDataFrequencyMaxValue(Math.max(...this.#dataFrequency ));
+    }
+
+    // Computes frequency using Rice Rule
+    computeFrequency()
     {
         const n = this.#datasetSize()
 
-        //The Rice Rule is presented as a simple alternative to Sturges' rule.
+        // The Rice Rule
         let allBarsCount = Math.ceil(2 * Math.cbrt(n));
 
         let range = this.#datasetMaxValue() - this.#datasetMinValue();
+        // To ensure there be one bar if input is only a single value
         if(range === 0){
             range = 1;
         }
@@ -169,13 +197,10 @@ class Histogram {
 
         const dataFrequencyMap = new Map();
 
-        // let maxBinIndex = 0;
         for (let i = 0; i < this.#datasetSize(); i++) {
             let binIndex =
                 Math.floor((this.#dataset[i] - this.#datasetMinValue() ) / binWidth);
-            // if (maxBinIndex < binIndex){
-            //     maxBinIndex = binIndex
-            // }
+
             if(dataFrequencyMap.has(binIndex)){
                 const val = dataFrequencyMap.get(binIndex) + 1;
                 dataFrequencyMap.set(binIndex, val);
@@ -186,31 +211,21 @@ class Histogram {
 
         }
 
-        this.#dataFrequency = Array(this.#maxBinIndex + 1).fill(0);
+        const dataFrequency = Array(this.#maxBinIndex + 1).fill(0);
 
         for(let i = 0; i <= this.#maxBinIndex; i++ ){
             if(dataFrequencyMap.has(i)){
-                this.#dataFrequency[i] = dataFrequencyMap.get(i);
+                dataFrequency[i] = dataFrequencyMap.get(i);
             }else{
-                this.#dataFrequency[i] = 0;
+                dataFrequency[i] = 0;
             }
         }
 
+        return dataFrequency
+
     }
 
-
-    updateDatasetAndDataFrequency(newDataset)
-    {
-        this.#dataset = this.#dataset.concat(...newDataset);
-        this.#updateDatasetMaxValue(Math.max(...this.#dataset))
-        this.#updateDatasetMinValue(Math.min(...this.#dataset));
-
-        this.calculateFrequency();
-        this.#updateDataFrequencyMinValue(Math.min(...this.#dataFrequency ));
-        this.#updateDataFrequencyMaxValue(Math.max(...this.#dataFrequency ));
-    }
-
-
+    // Clears Canvas
     clearCanvas()
     {
         this.#ctx.clearRect(
@@ -220,12 +235,12 @@ class Histogram {
             this.#canvasHeight());
     }
 
-
+    // Clears Dataset
     clearDataset(){
         this.#dataset = [];
     }
 
-
+    // draws x-axis line
     drawXAxis(){
         this.#drawOneBar(
             this.barOffsetFromXAxis,
@@ -234,7 +249,7 @@ class Histogram {
             this.axisLinesWidth);
     }
 
-
+    // Draws y-axis line
     drawYaxis(){
         this.#drawOneBar(
             this.yAxisOffsetFromCanvasXOrigin,
@@ -244,7 +259,7 @@ class Histogram {
         );
     }
 
-
+    // Draws both x and y axis
     drawAxisLines()
     {
         ctx.fillStyle = '#7f71fe';
@@ -252,84 +267,90 @@ class Histogram {
         this.drawYaxis()
     }
 
+    // Draws both x and y axis numbers
+    drawAxisNumbers(){
+        this.#drawYAxisNumbers();
+        this.#drawXAxisNumbers();
+    }
 
+    // get Array dataset size
     #datasetSize()
     {
         return this.#dataset.length;
     }
 
-
+    // get Array dataset max value
     #datasetMaxValue()
     {
         return this.#datasetMax;
     }
 
-
+    // get Array dataset min value
     #datasetMinValue()
     {
         return this.#datasetMin;
     }
 
-
+    // Updates Array dataset max value
     #updateDatasetMaxValue(max)
     {
         this.#datasetMax = max;
     }
 
-
+    // Updates Array dataset min value
     #updateDatasetMinValue(min)
     {
         this.#datasetMin = min;
     }
 
-
+    // Get Sorted Array frequency data min value
     #dataFrequencySize()
     {
         return this.#dataFrequency.length;
     }
 
-
+    // Get Sorted Array frequency data max value
     #dataFrequencyMaxValue()
     {
         return this.#dataFrequencyMax;
     }
 
-
+    // Updates Sorted Array frequency data max value
     #updateDataFrequencyMaxValue(max)
     {
         this.#dataFrequencyMax = max;
     }
 
-
+    // Updates Sorted Array frequency data min value
     #updateDataFrequencyMinValue(min)
     {
         this.#dataFrequencyMin = min;
     }
 
-
+    // Updates Canvas Width and Height
     updateCanvasSize(width, height)
     {
         this.#canvasWidthVal = width - this.canvasWidthOffset;
         this.#canvasHeightVal = height;
     }
 
-
+    // Get Canvas Width
     #canvasWidth(){
         return this.#canvasWidthVal;
     }
 
-
+    // Get Canvas Height
     #canvasHeight(){
         return this.#canvasHeightVal;
     }
 
-
+    // Draws a rectangle
     #drawOneBar(StartXCoordinate, startYCoordinate, barWidth, barHeight)
     {
         this.#ctx.fillRect(StartXCoordinate, startYCoordinate, barWidth, barHeight);
     }
 
-
+    // Randomly selects a number and applies to Canvas Context
     #selectBinRandomColor()
     {
         const red = 255*Math.random()|0;
@@ -339,8 +360,8 @@ class Histogram {
         this.#ctx.fillStyle = "rgb(" + red + "," + green + "," + blue + ")";
     }
 
-
-    #drawXAxisLabels()
+    // Draws x axis numbers relative to Sorted Array data frequency size
+    #drawXAxisNumbers()
     {
         ctx.fillStyle = "white";
         ctx.font = '1.7vw Arial';
@@ -354,7 +375,7 @@ class Histogram {
         }
 
         let x = this.barOffsetFromYAxis;
-        for (let binNum = 0; binNum <= this.#dataFrequencySize(); binNum++) {
+        for (let barNum = 0; barNum <= this.#dataFrequencySize(); barNum++) {
             xLabelValue = Math.round(xLabelValue * 1000) / 1000;
             ctx.fillText(
                 xLabelValue.toString(),
@@ -371,20 +392,20 @@ class Histogram {
         }
     }
 
-
-    #drawYAxisLabels()
+    // Draw Y axis Numbers
+    #drawYAxisNumbers()
     {
         ctx.fillStyle = "white";
         ctx.font = '25px Arial';
 
         let yValue = 0;
-        let valueStepSize = this.#dataFrequencyMaxValue() / this.yAxisBinsCount;
+        let valueStepSize = this.#dataFrequencyMaxValue() / this.yAxisLineCount;
 
         let yPosition = this.#canvasHeight() - this.barOffsetFromXAxis;
         let positionStepSize =
-            (yPosition - this.barOffsetFromCanvasOrigin) / this.yAxisBinsCount;
+            (yPosition - this.barOffsetFromCanvasOrigin) / this.yAxisLineCount;
 
-        for(let i = 0; i <= this.yAxisBinsCount; i++){
+        for(let i = 0; i <= this.yAxisLineCount; i++){
             yValue = Math.round(yValue * 100) / 100;
             ctx.fillText(yValue.toString(), 10 , yPosition + 10, 50);
             this.#drawOneBar(
@@ -398,7 +419,7 @@ class Histogram {
         }
     }
 
-
+    // Get bars width relative to Canvas width and Sorted Array data Frequency
     #barsWidth(){
         return (this.#canvasWidth()-this.barOffsetFromYAxis) / this.#dataFrequencySize();
     }
